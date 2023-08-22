eb113b105327cbb875ac3fcdfd20fcfe901c6a92
SAJAL <75297054+sajalmittal1781@users.noreply.github.com>
2023-08-22 21:39:22 +0530

    Updates

---
+++ precache-manifest.2ddc179dc3639e84aa8f176b8d9989c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63adfe196c7e90c3b912341431e95892",
+    "url": "/Blogs/index.html"
+  },
+  {
+    "revision": "1944463de10ba928bf04",
+    "url": "/Blogs/static/css/main.e479dddb.chunk.css"
+  },
+  {
+    "revision": "ac0a230c8537fa7531ee",
+    "url": "/Blogs/static/js/2.a960e375.chunk.js"
+  },
+  {
+    "revision": "1944463de10ba928bf04",
+    "url": "/Blogs/static/js/main.7f3ad5c3.chunk.js"
+  },
+  {
+    "revision": "76c499d327720c70361a",
+    "url": "/Blogs/static/js/runtime-main.5cd33c85.js"
+  }
+]);
